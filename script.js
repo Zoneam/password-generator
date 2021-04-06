fbbb875cb33fb9864179715af6cb2376b0428933
Hayk Mnatsakanyan <zoneam@gmail.com>
2021-04-06 16:19:44 -0700

    added checkboxes and input

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+  if (document.getElementById("upper-case").checked == true) {
+    console.log("true");
+  } else 
+  console.log("false");
+
+
+
+
+function generate() {
+  
+}
+
+
+
+
+
+
+
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -8,7 +30,14 @@ function writePassword() {
 
   passwordText.value = password;
 
+
+
+
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
