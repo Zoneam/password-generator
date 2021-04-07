22ab7cadb7f827dae8d895af2b42abd03eb6441d
Hayk Mnatsakanyan <zoneam@gmail.com>
2021-04-07 08:28:23 -0700

    adding readme file, fixed range erroer

--- script.js
+++ script.js
@@ -1,40 +1,36 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
-  // if (document.getElementById("upper-case").checked == true) {
-  //   console.log("true");
-  // } else 
-  // console.log("false");
-
 function generatePassword() {
   let lowerCase = "abcdefghijklmnopqrstuvwxyz";
-  let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   let numbers = "0123456789";
   let specialCharacters = "!#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
   let passwordCharacters = lowerCase;
   let password ="";
 
-if (document.getElementById("upper-case").checked == true) {
-  passwordCharacters+= upperCase;
-}
- if (document.getElementById("numeric").checked == true){
-  passwordCharacters+= numbers;
-}
-
-if (document.getElementById("special-characters").checked == true){
-  passwordCharacters+= specialCharacters;
-}
 
 
+  if (document.getElementById("password-lenght").value>=8 && document.getElementById("password-lenght").value<=128){
+      if (document.getElementById("upper-case").checked == true) {
+      passwordCharacters+= passwordCharacters.toUpperCase();
+      }
+      if (document.getElementById("numeric").checked == true){
+      passwordCharacters+= numbers;
+      }
+      if (document.getElementById("special-characters").checked == true){
+      passwordCharacters+= specialCharacters;
+      }
 
-  for (i=0;  i<24; i++ ){
-    password += passwordCharacters.charAt(Math.floor(Math.random()*passwordCharacters.length));
+      for (i=0;  i<document.getElementById("password-lenght").value; i++ ){
+        password += passwordCharacters.charAt(Math.floor(Math.random()*passwordCharacters.length));
+      }
+
+      console.log(passwordCharacters);
+      return password;
+  } else {
+ alert("Not a valid entery. Please enter password lenght from 8-128 range");
+ return("Not a valid entry!");
   }
-
-  console.log(password);
-  return password;
-
 
 }
 
