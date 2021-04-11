097e5a0753e1b22adc28e5bd0dccb36c767b4ced
Hayk Mnatsakanyan <zoneam@gmail.com>
2021-04-11 14:35:26 -0700

    added password strenght indicator and readme

--- readme.md
+++ readme.md
@@ -1,1 +1,48 @@
 ## Password Generator
+
+---
+
+---
+
+## Acceptance Criteria for Password Generator
+
+```
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN prompted for character types to include in the password
+THEN I choose lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+```
+
+## Have confirmed with instructor to use checkboxes instead of confirm prompts
+
+#### JS
+
+     [x] generatePassword() function to generate password
+     [x] Declared each character set as string
+     [x] Setting our password selected characters and our output password to empty string
+     [x] Checking to see if at least one of the options is selected
+     [x] If none is selected than alert user to select one of the options
+     [x] Checking if our password length meets requirement 8-128 characters
+     [x] Checking to see which options are selectet and adding character sets to our passwordCharacters
+     [x] Running for loop and randomly choosing from our passwordCharacters
+     [x] -Bonus- Running our password strenght indicator function
+     [x] Returning our password
+     [x] If the lenght is not in the range alert user
+     [x] writePassword() displaying password
+     [x] -Bonus- passwordStrenght() checks and displays password strenght
+     [x] -Bonus- Added event listener on password text area, when clicked password is copied to clipboard
+
+---
+
+![My Portfolio Page Screenshot](./screenshot/password-generator.png)
--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var myPassword = document.getElementById("password");
+var passwordLow = document.getElementById("password-strenght-low");
+var passwordMed = document.getElementById("password-strenght-med");
+var passwordHigh = document.getElementById("password-strenght-high");
+var strenghtVal = 0;
+
 function generatePassword() {
+  strenghtVal = 0;
+  passwordLow.style.visibility="hidden";
+  passwordMed.style.visibility="hidden";
+  passwordHigh.style.visibility="hidden";
   //charset declaration 
   let lowerCase = "abcdefghijklmnopqrstuvwxyz";
   let numbers = "0123456789";
@@ -23,20 +32,24 @@ function generatePassword() {
     //checking and adding character strings to our passwordCharacters holder
      if (document.getElementById("lower-case").checked == true){
       passwordCharacters+= lowerCase;
+      strenghtVal += 1;
      } 
     if (document.getElementById("upper-case").checked == true) {
       passwordCharacters+= lowerCase.toUpperCase();
       }
       if (document.getElementById("numeric").checked == true){
       passwordCharacters+= numbers;
+      strenghtVal += 1;
       }
       if (document.getElementById("special-characters").checked == true){
       passwordCharacters+= specialCharacters;
+      strenghtVal += 2;
       }
       //randomly selectring characters from our character set and adding to our password 
       for (i=0;  i < document.getElementById("password-length").value; i++ ){
         password += passwordCharacters[Math.floor(Math.random()*passwordCharacters.length)];
       }
+      passwordStrenght();
       //returning our password
       return password;
   } else {
@@ -55,10 +68,26 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
+
+//displaying password strenght
+function passwordStrenght(){
+  console.log(strenghtVal)
+  if (strenghtVal == 1) {
+    passwordLow.style.visibility="visible";
+  } else if (strenghtVal == 2){
+    passwordLow.style.visibility="visible";
+    passwordMed.style.visibility="visible";
+  } else if (strenghtVal > 2){
+    passwordLow.style.visibility="visible";
+    passwordMed.style.visibility="visible";
+    passwordHigh.style.visibility="visible";
+  }
+
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-//coping password 
+//coping password to clipboard
 document.querySelector("#password").addEventListener("click", function(){
   if (myPassword.value != "Not a valid entry!" && myPassword.value != "Please check one of the options!" && myPassword.value != ""){
   myPassword.select();
