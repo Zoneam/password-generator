273f0a8b1b50bae6d8b3c243aa8a5c2d1325d9cc
Hayk Mnatsakanyan <zoneam@gmail.com>
2021-04-11 13:10:41 -0700

    added click to copy functionality

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var myPassword = document.getElementById("password");
 function generatePassword() {
+  //charset declaration 
   let lowerCase = "abcdefghijklmnopqrstuvwxyz";
   let numbers = "0123456789";
   let specialCharacters = "!#:$%'()=+,-./;{<>*~?@_&[\\]^`|}";
-// let passwordChars =["abcdefghijklmnopqrstuvwxyz", "0123456789", "!#:$%'()=+,-./;{<>*~?@_&[\\]^`|}" ];
+  //resetting password characters 
   let passwordCharacters = "";
   let password ="";
+  //checking if none of the checkboxes are checked
   if    (document.getElementById("lower-case").checked == false && 
         document.getElementById("upper-case").checked == false && 
         document.getElementById("special-characters").checked == false && 
-        document.getElementById("special-characters").checked == false){
-       
+        document.getElementById("numeric").checked == false){
+       //if none of the checkboxes are checked show alert message
        alert("Please check one of the options!");
        return("Please check one of the options!");
   }
+  //checking if entered lenght of password meets required range 8-128 
   if (document.getElementById("password-length").value >= 8 && document.getElementById("password-length").value <= 128){
+    //checking and adding character strings to our passwordCharacters holder
      if (document.getElementById("lower-case").checked == true){
       passwordCharacters+= lowerCase;
      } 
-    
     if (document.getElementById("upper-case").checked == true) {
       passwordCharacters+= lowerCase.toUpperCase();
       }
@@ -30,13 +33,14 @@ function generatePassword() {
       if (document.getElementById("special-characters").checked == true){
       passwordCharacters+= specialCharacters;
       }
-
+      //randomly selectring characters from our character set and adding to our password 
       for (i=0;  i < document.getElementById("password-length").value; i++ ){
         password += passwordCharacters[Math.floor(Math.random()*passwordCharacters.length)];
       }
-      console.log(passwordCharacters);
+      //returning our password
       return password;
   } else {
+    //if not in required range alert 
     document.getElementById("password-length").style.backgroundColor = "red";
  alert("Not a valid entery. Please enter password length from 8-128 range");
  return("Not a valid entry!");
@@ -54,6 +58,14 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+//coping password 
+document.querySelector("#password").addEventListener("click", function(){
+  if (myPassword.value != "Not a valid entry!" && myPassword.value != "Please check one of the options!" && myPassword.value != ""){
+  myPassword.select();
+  document.execCommand("copy");
+  alert("Password Copied");
+  }
+})
 
 
 
